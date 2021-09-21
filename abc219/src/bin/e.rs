7d034991021fff8439729e611508aba9fbcbb8d1
shimo <shimo.8810@gmail.com>
2021-09-21 20:46:31 +0900

    [add]

--- abc219/src/bin/e.rs
+++ abc219/src/bin/e.rs
@@ -6,32 +6,49 @@ fn main() {
     input! {
         A: [[u8;4];4]
     }
-
     let mut ans = 0;
-    let n: u8 = A.iter().flatten().sum();
-    for bit in 0..(1 << 16usize) {
-        let p: Vec<_> = (0..16usize)
+
+    let n: u8 = A.iter().flatten().sum(); // number of village
+
+    for bit in 1..(1 << 16usize) {
+        let moats: Vec<_> = (0..16usize)
             .filter(|x| bit & (1 << x) != 0)
             .map(|x| (x / 4, x % 4))
             .collect();
 
-        if p.len() == 0 {
+        let mut stack = Vec::new();
+        let mut set = HashSet::new();
+
+        // check 2
+        let m = moats.iter().filter(|(x, y)| A[*y][*x] == 1).count() as u8;
+        if m != n {
             continue;
         }
 
-        // チェック
-        let s: HashSet<_> = [p[0]].iter().collect();
-        loop {
-            
-            for i in p.iter() {
+        //
+        stack.push(moats[0]);
+        set.insert(moats[0]);
+        while let Some((x, y)) = stack.pop() {
+            for &(t, u) in moats.iter() {
+                if set.contains(&(t, u)) {
+                    continue;
+                }
 
+                if ((x == t) && (y == 1 + u || y + 1 == u))
+                    || ((y == u) && (x == 1 + t || x + 1 == t))
+                {
+                    stack.push((t, u));
+                    set.insert((t, u));
+                }
             }
         }
 
-        let m = p.iter().filter(|(x, y)| A[*y][*x] == 1).count() as u8;
-        if m == n {
-            ans += 1;
+        if set.len() != moats.len() {
+            continue;
         }
+
+        ans += 1;
     }
+
     println!("{}", ans);
 }
