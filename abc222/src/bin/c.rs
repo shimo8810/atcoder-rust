599e5c3657744ead2d747da900758551c3217dc1
shimo <shimo.8810@gmail.com>
2021-10-10 13:21:40 +0900

    [update] abc222 c

--- abc222/src/bin/c.rs
+++ abc222/src/bin/c.rs
@@ -1,38 +1,15 @@
 use proconio::{input, marker::Chars};
-use std::collections::HashMap;
 
-fn judge(x: char, y: char) -> (usize, usize) {
-    if x == 'G' {
-        if y == 'G' {
-            (0, 0)
-        } else if y == 'C' {
-            (1, 0)
-        } else {
-            // P
-            (0, 1)
-        }
-    } else if x == 'C' {
-        if y == 'G' {
-            (0, 1)
-        } else if y == 'C' {
-            (0, 0)
-        } else {
-            // P
-            (1, 0)
-        }
+fn judge(x: char, y: char) -> Option<usize> {
+    if x == y {
+        None
+    } else if (x == 'G' && y == 'C') || (x == 'C' && y == 'P') || (x == 'P' && y == 'G') {
+        Some(0)
     } else {
-        // P
-        if y == 'G' {
-            (1, 0)
-        } else if y == 'C' {
-            (0, 1)
-        } else {
-            // P
-            (0, 0)
-        }
+        Some(1)
     }
-    // return (0, 0);
 }
+
 #[allow(non_snake_case)]
 fn main() {
     input! {
@@ -41,33 +18,22 @@ fn main() {
         A: [Chars; 2 * N]
     }
 
-    let mut dic = HashMap::new();
-    for i in 0..N {
-        dic.insert(2 * i, 0);
-        dic.insert(2 * i + 1, 0);
-    }
+    let mut r: Vec<_> = (0..(2 * N)).map(|x| (0, x)).collect();
 
     for m in 0..M {
-        //
-        let tmp = dic.clone();
-        let mut v: Vec<_> = tmp.iter().collect();
-        v.sort_by(|(&i, &x), (&j, &y)| (99 - j + 100 * y).cmp(&(99 - i + 100 * x)));
-
         for i in 0..N {
-            let (&ai, _) = v[2 * i];
-            let (&bi, _) = v[2 * i + 1];
-            let (da, db) = judge(A[ai][m], A[bi][m]);
-            let aa = dic.entry(ai).or_insert(0);
-            *aa += da;
-            let bb = dic.entry(bi).or_insert(0);
-            *bb += db;
+            //
+            let a = r[2 * i];
+            let b = r[2 * i + 1];
+
+            if let Some(j) = judge(A[a.1][m], A[b.1][m]) {
+                r[2 * i + j].0 -= 1;
+            }
         }
+        r.sort_unstable();
     }
 
-    let mut v: Vec<_> = dic.iter().collect();
-    v.sort_by(|(&i, &x), (&j, &y)| (99 - j + 100 * y).cmp(&(99 - i + 100 * x)));
-
-    for (&i, _) in v.iter() {
+    for (_, i) in r.iter() {
         println!("{}", i + 1);
     }
 }
