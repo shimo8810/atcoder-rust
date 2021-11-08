c0d2d2e1e16acb1b5e6e643d30a5e19d37bf3ba9
shimo8810 <shimo.8810@gmail.com>
2021-11-08 22:25:52 +0900

    [add] typical90 075

--- typical90/src/bin/075.rs
+++ typical90/src/bin/075.rs
@@ -1,3 +1,27 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+      mut N: usize,
+    }
+
+    let mut n = 0;
+
+    for i in 2.. {
+        if i * i > N {
+            break;
+        }
+        while N % i == 0 {
+            N /= i;
+            n += 1;
+        }
+    }
+    if N != 1 {
+        n += 1;
+    }
+
+    let ans = (0..).find(|&x| (1 << x) >= n).unwrap();
+    println!("{}", ans);
 }
