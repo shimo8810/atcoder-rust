197d0a7beec1a1ee88179210e463a3713d30ec19
shimo8810 <shimo.8810@gmail.com>
2021-11-09 00:25:49 +0900

    [add] add typical90

--- typical90/src/bin/079.rs
+++ typical90/src/bin/079.rs
@@ -1,3 +1,32 @@
+use proconio::{fastout, input};
+
+const YES: &str = "Yes";
+const NO: &str = "No";
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+      H: usize,
+      W: usize,
+      mut A: [[i32; W]; H],
+      B: [[i32; W]; H]
+    }
+
+    let mut ans = 0;
+    for y in 0..(H - 1) {
+        for x in 0..(W - 1) {
+            let d = B[y][x] - A[y][x];
+            ans += d.abs() as u64;
+            A[y][x] += d;
+            A[y][x + 1] += d;
+            A[y + 1][x] += d;
+            A[y + 1][x + 1] += d;
+        }
+    }
+    if A == B {
+        println!("{}\n{}", YES, ans);
+    } else {
+        println!("{}", NO);
+    }
 }
--- typical90/src/bin/082.rs
+++ typical90/src/bin/082.rs
@@ -1,3 +1,26 @@
+use proconio::{fastout, input};
+use std::cmp::{max, min};
+
+const M: u64 = 1_000_000_007;
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+      L: u64,
+      R: u64
+    }
+
+    // let ln = ;
+    // let rn = R.to_string().len();
+    let mut ans = 0;
+    for n in L.to_string().len()..=R.to_string().len() {
+        // 同じ桁のやつを全部足す, n 桁数
+        let l = max(L, 10u64.pow((n - 1) as u32));
+        let r = min(R, 10u64.pow(n as u32) - 1);
+
+        ans = (ans + ((r - l + 1) % M) * ((l + r) % M) * n as u64 / 2) % M;
+    }
+
+    println!("{}", ans);
 }
