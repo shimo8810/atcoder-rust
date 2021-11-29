0db677d92240228a8f96d2dd629bf0313899e1fe
shimo8810 <shimo.8810@gmail.com>
2021-11-30 00:07:22 +0900

    [add] abc229

--- abc229/src/bin/c.rs
+++ abc229/src/bin/c.rs
@@ -1,16 +1,20 @@
 use proconio::{fastout, input};
-
-use std::cmp::max;
+use std::cmp::min;
 #[allow(non_snake_case)]
 #[fastout]
 fn main() {
     input! {
       N: usize,
-      W: usize,
-      AB: [(usize, usize); N]
+      mut W: u64,
+      mut AB: [(u64, u64); N]
     }
+    AB.sort_unstable();
 
-    
-
-    println!("{}", dp[N][W]);
+    let mut ans = 0;
+    for &(a, b) in AB.iter().rev() {
+        let k = min(W, b);
+        ans += k * a;
+        W -= k;
+    }
+    println!("{}", ans);
 }
