3f1c31fb6bfbb6f0528e7b1beea193ddaee88f9e
shimo8810 <shimo.8810@gmail.com>
2021-12-10 00:19:08 +0900

    [add] abc230

--- abc230/src/bin/e.rs
+++ abc230/src/bin/e.rs
@@ -1,12 +1,24 @@
 use proconio::{fastout, input};
 
+// fn solve_simple(n: u64) -> u64 {
+//     (1..=n).map(|x| n / x).sum()
+// }
 #[allow(non_snake_case)]
 #[fastout]
 fn main() {
     input! {
-      N: usize,
+      N: u64,
     }
 
-    let ans = 0;
+    let mut i = 1;
+    let mut ans = 0;
+    while i <= N {
+        let r = N / i;
+        let q = N % i;
+        let k = q / r + 1;
+        ans += r * k;
+        i += k;
+    }
+    // assert_eq!(ans, solve_simple(N));
     println!("{}", ans);
 }
--- arc127/src/bin/a.rs
+++ arc127/src/bin/a.rs
@@ -1,11 +1,18 @@
-use proconio::input;
+use proconio::{fastout, input};
 
 #[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    input!{
-    //
+    input! {
+      N: usize,
     }
-    let a = 11;
-    let ans = 0;
+    let s = N.to_string().len();
+
+    let mut ans = 0;
+    for i in 1..=s {
+        for j in 1..=i {
+
+        }
+    }
     println!("{}", ans);
 }
