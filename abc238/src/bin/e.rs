83ede289b0d783c7c172de124fd2bd5fe8af581d
shimo8810 <shimo.8810@gmail.com>
2022-02-12 15:14:17 +0900

    [add] abc238

--- abc238/src/bin/e.rs
+++ abc238/src/bin/e.rs
@@ -1,3 +1,39 @@
+use proconio::{fastout, input};
+use std::collections::HashSet;
+
+const YES: &str = "Yes";
+const NO: &str = "No";
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        Q: usize
+    }
+
+
+    // Graf作成
+    let mut graph = vec![vec![]; N + 1];
+    for _ in 0..Q {
+        input! {l: usize, r: usize}
+        graph[r].push(l - 1);
+        graph[l - 1].push(r);
+    }
+
+    let mut check = HashSet::new();
+
+    let mut que = vec![0];
+    while let Some(v) = que.pop() {
+        check.insert(v);
+        for u in &graph[v] {
+            if !check.contains(u) {
+                que.push(*u);
+            }
+        }
+    }
+
+    let ans = if check.contains(&N) { YES } else { NO };
+
+    println!("{}", ans);
 }
