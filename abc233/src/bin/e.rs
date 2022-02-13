ccdf9b72f12819583d0b8bf4e8f13938ff543705
shimo <shimo.8810@gmail.com>
2022-02-13 19:21:50 +0900

    [add] abc233

--- abc233/src/bin/e.rs
+++ abc233/src/bin/e.rs
@@ -1,3 +1,34 @@
+use proconio::{fastout, input, marker::Chars};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        X: Chars,
+    }
+
+    let xs: Vec<_> = X.iter().map(|&x| (x as u8 - b'0') as u64).collect();
+
+    let mut s: u64 = xs.iter().sum();
+    let mut c = 0;
+    let mut ans = vec![];
+    let mut i = xs.len() as i64 - 1;
+    loop {
+        c += s;
+        ans.push(((c % 10) as u8 + b'0') as char);
+        c /= 10;
+        if i >= 0 {
+            s -= xs[i as usize];
+        }
+
+        if i <= 0 && c == 0 {
+            break;
+        }
+        i -= 1;
+    }
+
+    let ans: String = ans.iter().rev().collect();
+    println!("{}", ans);
+
+    // let a = b'0';
 }
