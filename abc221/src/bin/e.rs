cc8a6b69f4ec405a9b602866563f8a29c2c75521
shimo8810 <shimo.8810@gmail.com>
2022-02-20 14:51:56 +0900

    [add] abc221

--- abc221/src/bin/e.rs
+++ abc221/src/bin/e.rs
@@ -1,14 +1,12 @@
-use proconio::input;
-
-const M: u64 = 998_244_353;
+use proconio::{fastout, input};
 
 #[allow(non_snake_case)]
+#[fastout]
 fn main() {
     input! {
         N: usize,
-        A: u64
     }
 
-    let ans = 0;
+    let mut ans = 0;
     println!("{}", ans);
 }
