334400023ac776aa9d1cf3a8cdb78d5f027a8c99
shimo8810 <shimo.8810@gmail.com>
2022-04-30 13:41:44 +0900

    [add] abc247

--- abc/abc247/src/bin/e.rs
+++ abc/abc247/src/bin/e.rs
@@ -1,36 +1,32 @@
+use proconio::{fastout, input};
 use std::collections::VecDeque;
 
-use proconio::{fastout, input};
+#[allow(non_snake_case)]
+fn shaku(A: &[u32], X: u32, Y: u32) -> usize {
+    let mut deq = VecDeque::new();
 
-fn shaku(s: &[u32], x: u32, y: u32) -> usize {
-    let mut h = 0;
-    let mut t = 0;
-    let mut numx = 0;
-    let mut numy = 0;
+    let mut cx = 0;
+    let mut cy = 0;
     let mut ans = 0;
+    for (i, &a) in A.iter().enumerate() {
+        deq.push_back(a);
+        if a == X {
+            cx += 1;
+        }
+        if a == Y {
+            cy += 1;
+        }
 
-    while h < s.len() {
-        while t < s.len() && (numx == 0 || numy == 0) {
-            if s[t] == x {
-                numx += 1;
+        while !deq.is_empty() && cx > 0 && cy > 0 {
+            ans += A.len() - i;
+            let b = deq.pop_front().unwrap();
+            if b == X {
+                cx -= 1;
             }
-            if s[t] == y {
-                numy += 1;
+            if b == Y {
+                cy -= 1;
             }
-            t += 1;
         }
-
-        if numx > 0 && numy > 0 {
-            ans += s.len() + 1 - t;
-        }
-
-        if s[h] == x {
-            numx -= 1;
-        }
-        if s[h] == y {
-            numy -= 1;
-        }
-        h += 1;
     }
 
     ans
@@ -45,22 +41,20 @@ fn main() {
         Y: u32,
         A: [u32; N]
     }
-    let mut seps: VecDeque<_> = A
-        .iter()
-        .enumerate()
-        .filter(|(_, &a)| a > X || Y > a)
-        .map(|(i, _)| i + 1)
-        .collect();
+    let mut lrs = vec![];
+    let mut tmp = 0;
+    for (i, &a) in A.iter().enumerate() {
+        if a < Y || X < a {
+            lrs.push((tmp, i));
+            tmp = i + 1;
+        }
+    }
+    lrs.push((tmp, A.len()));
 
-    seps.push_front(0);
-    seps.push_back(N + 1);
-    let seps: Vec<_> = seps.into_iter().collect();
-    // println!("{:?}", seps);
     let mut ans = 0;
-    for sep in seps.windows(2) {
-        let l = sep[0];
-        let r = sep[1] - 1;
+    for &(l, r) in &lrs {
         ans += shaku(&A[l..r], X, Y);
     }
+
     println!("{}", ans);
 }
