253814d9bcab42966c463c1d3b8224b3e7f2b0fb
shimo8810 <shimo.8810@gmail.com>
2022-05-09 22:58:30 +0900

    [add] abc245

--- abc/abc245/src/bin/e.rs
+++ abc/abc245/src/bin/e.rs
@@ -1,3 +1,16 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        (N, M): (usize, usize),
+        A: [u32; N],
+        B: [u32; N],
+        C: [u32; M],
+        D: [u32; M]
+    }
+
+    let mut ans = 0;
+    println!("{}", ans);
 }
