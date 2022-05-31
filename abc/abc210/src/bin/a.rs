ea1767bac3c8703ce0d898491645eca7b2e717c4
shimo8810 <shimo.8810@gmail.com>
2022-06-01 00:14:41 +0900

    [add] abc210

--- abc/abc210/src/bin/a.rs
+++ abc/abc210/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        N: i32,
+        A: i32,
+        X: i32,
+        Y: i32
+    }
+
+    let ans = A.min(N) * X + 0.max(N - A) * Y;
+    println!("{}", ans);
 }
