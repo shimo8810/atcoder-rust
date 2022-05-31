57efb17f2efb2bedc4fa4bf280952b7582b230fd
shimo8810 <shimo.8810@gmail.com>
2022-06-01 00:14:56 +0900

    [add] abc242

---
+++ abc/abc242/src/bin/e.rs
@@ -0,0 +1,14 @@
+use proconio::{fastout, input};
+
+const D: u32 = 998_244_353;
+
+#[allow(non_snake_case)]
+#[fastout]
+fn main() {
+    input! {
+        N: usize,
+    }
+
+    let mut ans = 0;
+    println!("{}", ans);
+}
