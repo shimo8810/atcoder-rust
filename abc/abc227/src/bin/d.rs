627f298918d6e8ee0832039e6f947b06a462f584
shimo8810 <shimo.8810@gmail.com>
2022-06-04 00:35:51 +0900

    [add] abc227

--- abc/abc227/src/bin/d.rs
+++ abc/abc227/src/bin/d.rs
@@ -1,3 +1,25 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        K: usize,
+        A:[usize; N]
+    }
+
+    let mut ok = 0;
+    let mut ng = std::usize::MAX / K;
+    while ng - ok > 1 {
+        let p = (ok + ng) / 2;
+
+        let s: usize = A.iter().map(|&a| a.min(p)).sum();
+        if p * K <= s {
+            ok = p;
+        } else {
+            ng = p;
+        }
+    }
+    println!("{}", ok);
 }
