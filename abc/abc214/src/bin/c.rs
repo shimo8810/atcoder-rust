6b1a5a3e2fad69b410ea700e387f9d2c5d559f75
shimo8810 <shimo.8810@gmail.com>
2022-06-04 07:53:03 +0900

    [add] abc214

--- abc/abc214/src/bin/c.rs
+++ abc/abc214/src/bin/c.rs
@@ -1,13 +1,31 @@
-use proconio::input;
+use proconio::{fastout, input};
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashSet};
 
 #[allow(non_snake_case)]
+#[fastout]
 fn main() {
     input! {
         N: usize,
-        S: [usize; N],
-        T: [usize; N]
+        S: [u32; N],
+        T: [u32; N]
     }
 
-    let ans = 0;
-    println!("{}", ans);
+    let mut heap = BinaryHeap::new();
+    let mut set = HashSet::new();
+    let mut ans = vec![std::u32::MAX; N];
+    for (i, &t) in T.iter().enumerate() {
+        heap.push((Reverse(t), i));
+    }
+    while let Some((Reverse(t), i)) = heap.pop() {
+        ans[i] = ans[i].min(t);
+        set.insert(i);
+        if set.len() == N {
+            break;
+        }
+        heap.push((Reverse(t + S[i]), (i + 1) % N));
+    }
+    for x in &ans {
+        println!("{}", x);
+    }
 }
--- abc/abc214/src/bin/d.rs
+++ abc/abc214/src/bin/d.rs
@@ -1,3 +1,71 @@
+use proconio::{fastout, input, marker::Usize1};
+
+#[allow(dead_code)]
+struct UFT {
+    par: Vec<usize>,
+    size: Vec<usize>,
+    n: usize,
+}
+
+#[allow(dead_code)]
+impl UFT {
+    fn new(n: usize) -> Self {
+        let par = (0..n).collect();
+        let size = vec![1; n];
+        Self { par, size, n }
+    }
+    fn find(&mut self, a: usize) -> usize {
+        if self.par[a] == a {
+            a
+        } else {
+            self.par[a] = self.find(self.par[a]);
+            self.par[a]
+        }
+    }
+    fn unite(&mut self, a: usize, b: usize) -> usize {
+        let mut x = self.find(a);
+        let mut y = self.find(b);
+        if x == y {
+            x
+        } else {
+            if self.size[x] < self.size[y] {
+                std::mem::swap(&mut x, &mut y);
+            }
+            self.size[x] += self.size[y];
+            self.par[y] = x;
+            x
+        }
+    }
+    fn same(&mut self, a: usize, b: usize) -> bool {
+        self.find(a) == self.find(b)
+    }
+
+    fn size(&mut self, a: usize) -> usize {
+        let z = self.find(a);
+        self.size[z]
+    }
+}
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+    }
+
+    let mut edges = vec![];
+    for _ in 1..N {
+        input! {u: Usize1, v: Usize1, w: usize}
+        edges.push((w, u, v));
+    }
+    let mut uft = UFT::new(N);
+
+    let mut ans = 0usize;
+    edges.sort_unstable();
+
+    for &(w, u, v) in &edges {
+        ans += w * uft.size(u) * uft.size(v);
+        uft.unite(u, v);
+    }
+    println!("{}", ans);
 }
