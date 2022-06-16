0529f64a83489252f89d3f3f842ae66d0d30f083
shimo8810 <shimo.8810@gmail.com>
2022-06-16 19:51:22 +0900

    [add] abc201

---
+++ abc/abc201/src/bin/a.rs
@@ -0,0 +1,14 @@
+use proconio::{fastout, input};
+const YES: &str = "Yes";
+const NO: &str = "No";
+#[allow(non_snake_case)]
+#[fastout]
+fn main() {
+    input! {
+        mut A: [u8; 3],
+    }
+
+    A.sort_unstable();
+    let ans = if A[2] - A[1] == A[1] - A[0] { YES } else { NO };
+    println!("{}", ans);
+}
