149de04b9b198b7f570c1502e7bd26dacd4d8890
shimo8810 <shimo.8810@gmail.com>
2022-09-10 12:34:34 +0900

    [add] abc261

--- abc/abc261/src/bin/c.rs
+++ abc/abc261/src/bin/c.rs
@@ -1,3 +1,20 @@
+use proconio::{fastout, input};
+use std::collections::HashMap;
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+    }
+    let mut map = HashMap::new();
+    for _ in 0..N {
+        input! {S: String}
+        if map.contains_key(&S) {
+            println!("{}({})", S, map.get(&S).unwrap());
+        } else {
+            println!("{}", S);
+        }
+        *map.entry(S).or_insert(0) += 1;
+    }
 }
--- abc/abc261/src/bin/d.rs
+++ abc/abc261/src/bin/d.rs
@@ -1,3 +1,36 @@
+use proconio::{fastout, input};
+use std::collections::HashMap;
+
+// 6 3
+// 2 7 1 8 2 8
+// 2 10
+// 3 1
+// 5 5
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        X: [u64; N],
+    }
+    let mut bonus = HashMap::new();
+    for _ in 0..M {
+        input! {C: usize, Y: u64}
+        bonus.insert(C, Y);
+    }
+    // dp(i, c) := i回コインを投げたとき、連続回数がcである最大値
+    let mut dp = vec![vec![0u64; N + 1]; N + 1];
+
+    for i in 1..=N {
+        // コインが裏のとき
+        dp[i][0] = *dp[i - 1].iter().max().unwrap();
+        for c in 1..=i {
+            dp[i][c] = dp[i - 1][c - 1] + X[i - 1] + bonus.get(&c).unwrap_or(&0);
+        }
+    }
+
+    let ans = dp[N].iter().max().unwrap();
+    println!("{}", ans);
 }
