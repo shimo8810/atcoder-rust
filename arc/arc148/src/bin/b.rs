2c722c13e95d0cb1d0a435f49a868e9c9630362d
shimo <shimo.8810@gmail.com>
2022-09-23 22:56:36 +0900

    [add] arc148

--- arc/arc148/src/bin/b.rs
+++ arc/arc148/src/bin/b.rs
@@ -1,11 +1,35 @@
-use num::integer::gcd;
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::Chars};
 
 #[allow(non_snake_case)]
 #[fastout]
 fn main() {
     input! {
-        N: usize,
+        n: usize,
+        s: Chars
+    }
+
+    let k = s.iter().position(|&x| x == 'p');
+
+    let mut ans = s.iter().collect::<String>();
+    if k.is_none() {
+        println!("{}", ans);
+        return;
+    }
+
+    let l = k.unwrap();
+
+    for r in l..n {
+        let head: String = s[..l].iter().collect();
+        let mid: String = s[l..=r]
+            .iter()
+            .rev()
+            .map(|&x| if x == 'p' { 'd' } else { 'p' })
+            .collect();
+        let tail: String = s[(r + 1)..].iter().collect();
+        let tmp = format!("{}{}{}", head, mid, tail);
+        if tmp < ans {
+            ans = tmp;
+        }
     }
 
     println!("{}", ans);
